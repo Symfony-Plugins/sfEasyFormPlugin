c7b1609a560ff033461f4be8fe8e01391550983e
hartym <hartym@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-04-09 10:26:04 -0500

    [sfEasyFormPlugin] fixed rendering of hidden fields
    git-svn-id: http://svn.symfony-project.com/plugins/sfEasyFormPlugin/trunk@17166 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfEasyForm/templates/_formSchema.php
+++ modules/sfEasyForm/templates/_formSchema.php
@@ -1,91 +1,100 @@
 <?php
-$field = sfOutputEscaper::unescape($field);
 
-/* global errors */
-if ($field instanceof sfForm)
-{
-  echo $field->renderGlobalErrors();
-  echo $field->renderHiddenFields();
-  echo '<ul class="form-root">';
-}
-else
-{
-  echo $field->renderLabel();
-  echo '<ul class="form-child">';
-}
+  $field = sfOutputEscaper::unescape($field);
 
-/* custom renderer foreach childs */
-if (isset($decorators[$starPath=$path.'/*']))
-{
-  if ($decorators[$starPath])
+  /* global errors */
+  if ($field instanceof sfForm)
   {
-    foreach ($field as $_name => $_field)
-    {
-      include_partial($decorators[$starPath], array(
-        'fieldName'  => $_name,
-        'field'      => $_field,
-        'path'       => $path.'/'.$_name,
-        'decorators' => $decorators,
-        'form'       => $form,
-      ));
-    }
+    echo $field->renderGlobalErrors();
+    echo '<ul class="form-root">';
   }
-}
-else
-{
-  if (isset($decorators[$dirPath=$path.'/']))
+  else
   {
-    if ($decorators[$dirPath])
+    echo $field->renderLabel();
+    echo '<ul class="form-child">';
+  }
+
+  $hiddenBuffer = '';
+
+  /* custom renderer foreach childs */
+  if (isset($decorators[$starPath=$path.'/*']))
+  {
+    if ($decorators[$starPath])
     {
-      include_partial($decorators[$dirPath], array(
-        'fieldName'  => $fieldName,
-        'field'      => $field,
-        'path'       => $path,
-        'decorators' => $decorators,
-        'form'       => $form,
-      ));
+      foreach ($field as $_name => $_field)
+      {
+        include_partial($decorators[$starPath], array(
+          'fieldName'  => $_name,
+          'field'      => $_field,
+          'path'       => $path.'/'.$_name,
+          'decorators' => $decorators,
+          'form'       => $form,
+        ));
+      }
     }
   }
   else
   {
-    foreach ($field as $_name => $_field)
+    if (isset($decorators[$dirPath=$path.'/']))
     {
-      $_class = get_class($_field);
-
-      $_context = array(
-        'fieldName'  => $_name,
-        'field'      => $_field,
-        'path'       => $path.'/'.$_name,
-        'decorators' => $decorators,
-        'form'       => $form,
-      );
-
-      if (isset($decorators[$newPath=$path.'/'.$_name]))
+      if ($decorators[$dirPath])
       {
-        if ($decorators[$newPath])
-        {
-          include_partial($decorators[$newPath], $_context);
-        }
+        include_partial($decorators[$dirPath], array(
+          'fieldName'  => $fieldName,
+          'field'      => $field,
+          'path'       => $path,
+          'decorators' => $decorators,
+          'form'       => $form,
+        ));
       }
-      else
+    }
+    else
+    {
+      foreach ($field as $_name => $_field)
       {
-        if ($_class=='sfFormField')
+        $_class = get_class($_field);
+
+        $_context = array(
+          'fieldName'  => $_name,
+          'field'      => $_field,
+          'path'       => $path.'/'.$_name,
+          'decorators' => $decorators,
+          'form'       => $form,
+        );
+
+        if (isset($decorators[$newPath=$path.'/'.$_name]))
         {
-          if (!$_field->isHidden())
+          if ($decorators[$newPath])
           {
-            echo '<li>'.$_field->renderError().$_field->renderLabel().$_field->render().'</li>';
+            include_partial($decorators[$newPath], $_context);
           }
-        }
-        elseif ($_class=='sfFormFieldSchema')
-        {
-          echo '<li>'.get_partial(isset($decorators['@schema'])?$decorators['@schema']:'sfEasyForm/formSchema', $_context).'</li>';
         }
         else
         {
-          throw new Exception(sprintf('Rendering of «%s» is not implemented', $_class));
+          if ($_class=='sfFormField')
+          {
+            if ($_field->isHidden())
+            {
+              $hiddenBuffer .= $_field->render();
+            }
+            else
+            {
+              echo '<li>'.$_field->renderError().$_field->renderLabel().$_field->render().'</li>';
+            }
+
+          }
+          elseif ($_class=='sfFormFieldSchema')
+          {
+            echo '<li>'.get_partial(isset($decorators['@schema'])?$decorators['@schema']:'sfEasyForm/formSchema', $_context).'</li>';
+          }
+          else
+          {
+            throw new Exception(sprintf('Rendering of «%s» is not implemented', $_class));
+          }
         }
       }
     }
   }
-}
+
 ?></ul>
+<?php echo $hiddenBuffer; ?>
